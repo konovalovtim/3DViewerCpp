aaaa5845d81b12292273d36a3f53d8eab5d4cfcd
Timur <timkonovalov9858@gmail.com>
2023-04-28 23:11:04 +0300

    mediator init

--- src/model/VertexesAndFacets.h
+++ src/model/VertexesAndFacets.h
@@ -7,10 +7,37 @@ namespace s21 {
 /**
  * @brief Класс для хранения данных об Объекте
  */
+// class VertexesAndFacets {
+// public:
+//   std::vector<std::vector<unsigned>> facets{}; ///< Вектор фигур
+//   std::vector<double> vertexes{};              ///< Вектор вершин
+// };
+
+// class Mediator {
+//   VertexesAndFacets *vertexesAndFacets;
+// };
+
+// class VertexesAndFacets;
 class VertexesAndFacets {
-public:
+ public:
   std::vector<std::vector<unsigned>> facets{}; ///< Вектор фигур
   std::vector<double> vertexes{};              ///< Вектор вершин
+};
+
+/**
+ * Базовый Компонент обеспечивает базовую функциональность хранения экземпляра
+ * посредника внутри объектов компонентов.
+ */
+class Mediator {
+ protected:
+  VertexesAndFacets *vertexesAndFacets_;
+
+ public:
+  Mediator(VertexesAndFacets *vertexesAndFacets = nullptr) : vertexesAndFacets_(vertexesAndFacets) {
+  }
+  void set_mediator(VertexesAndFacets *vertexesAndFacets) {
+    this->vertexesAndFacets_ = vertexesAndFacets;
+  }
 };
 
 } // namespace s21
