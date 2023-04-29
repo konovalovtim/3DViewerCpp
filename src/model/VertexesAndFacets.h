3b1ecf76b58c892d45be528885748a9cec241fae
Timur <timkonovalov9858@gmail.com>
2023-04-29 11:14:47 +0300

    mediator

--- src/model/VertexesAndFacets.h
+++ src/model/VertexesAndFacets.h
@@ -1,45 +1,25 @@
 #ifndef CPP4_3DVIEWER_V2_0_1_PART_OBJECT_H
 #define CPP4_3DVIEWER_V2_0_1_PART_OBJECT_H
 
+#include <list>
 #include <vector>
 
 namespace s21 {
 /**
  * @brief Класс для хранения данных об Объекте
  */
-// class VertexesAndFacets {
-// public:
-//   std::vector<std::vector<unsigned>> facets{}; ///< Вектор фигур
-//   std::vector<double> vertexes{};              ///< Вектор вершин
-// };
-
-// class Mediator {
-//   VertexesAndFacets *vertexesAndFacets;
-// };
-
-// class VertexesAndFacets;
 class VertexesAndFacets {
  public:
-  std::vector<std::vector<unsigned>> facets{}; ///< Вектор фигур
-  std::vector<double> vertexes{};              ///< Вектор вершин
+  std::vector<std::vector<unsigned>> facets{};  // Вектор фигур
+  std::vector<double> vertexes{};               // Вектор вершин
 };
 
-/**
- * Базовый Компонент обеспечивает базовую функциональность хранения экземпляра
- * посредника внутри объектов компонентов.
- */
 class Mediator {
- protected:
-  VertexesAndFacets *vertexesAndFacets_;
-
- public:
-  Mediator(VertexesAndFacets *vertexesAndFacets = nullptr) : vertexesAndFacets_(vertexesAndFacets) {
-  }
-  void set_mediator(VertexesAndFacets *vertexesAndFacets) {
-    this->vertexesAndFacets_ = vertexesAndFacets;
-  }
+  public:
+  VertexesAndFacets model;
+  std::vector<Mediator *> mediator;
 };
 
-} // namespace s21
+}  // namespace s21
 
-#endif // CPP4_3DVIEWER_V2_0_1_PART_OBJECT_H
+#endif  // CPP4_3DVIEWER_V2_0_1_PART_OBJECT_H
